3cd6f26d4fbbb485976da4924cfb825c09c719f9
lucerohospina <lucerojhp@gmail.com>
2018-01-13 12:37:13 -0500

    html y js cambiado para tweetear por medio de jquery

--- js/app.js
+++ js/app.js
@@ -1,62 +1,43 @@
-window.addEventListener('load', function () {
-    var textArea = document.getElementById('text-area');
-    var tweetBtn = document.getElementById('tweet-btn');
-    var tweetsContainer = document.getElementById('tweets-container');
-    var counter = document.getElementById("counter");
-    var max = 140;
-    
-  tweetBtn.addEventListener("click", function() {
-    
+$(document).ready(function() {
+  // declarando variables
+  var $inputTweets = $('#input-tweets');
+  var $btn = $('#tweet-btn');
+  var $tweets = $('#tweets-container');
+  var $counter = $('#counter');
+  var max = 140;
 
-    // if(newTweet.textContent == " ") {
-    //   tweetBtn.disabled = true;
-    // } else if(newTweet.textContent = textArea.value) {
-    //   tweetBtn.disabled = false;
-    //   tweetsContainer.insertBefore(newTweet, tweetsContainer.childNodes[0]);
-    //   // tweetBtn.classList.add("able");
-    // }
+  //damos evento al boton y al textarea
+  $inputTweets.on('input', writtingTweets);
+  $btn.on('click', tweet);
 
-    if(textArea.value) {
-      var newTweet = document.createElement("div");
-      newTweet.classList.add("container");
-      tweetsContainer.insertBefore(newTweet, tweetsContainer.childNodes[0]);
-      newTweet.textContent = textArea.value;
-    
-      textArea.value = "";
-      textArea.focus();
-      
-    } else {
-      tweetBtn.disabled = true;
-      
-    }
-  })
+ function writtingTweets() {
+   console.log('input');
+   if($inputTweets.val()) {
+     $btn.removeAttr('disabled');
+     $btn.addClass('able');
+   } else {
+     $btn.attr('disabled', true);
+   }
+ }
 
-  textArea.addEventListener("keyup", function() {
-    var characters = textArea.value.length;
-    counter.textContent = max - characters;
+ function tweet() {
+   console.log('click');
+   if($inputTweets.val()){
+    $tweets.prepend('<div class="jumbotron"><p class="new-tweet"></p></div>');
+    $('.new-tweet').first().append($inputTweets.val());
+     $inputTweets.val('');
+     $inputTweets.focus();
+     $btn.attr('disabled', true);
+   }
+ }
 
-    if(characters>0 && characters<120) {
-      tweetBtn.disabled = false;
-      tweetBtn.classList.add("able");
-      counter.style.color = "rgb(80,182,245)";
-    } else if(characters>=120 && characters<130) {
-      tweetBtn.disabled = false;
-      tweetBtn.classList.add("able");
-      counter.style.color = "yellow";
-    } else if(characters>=130 && characters<=140) {
-      tweetBtn.disabled = false;
-      tweetBtn.classList.add("able");
-      counter.style.color = "orange";
-    } else if(characters>140) {
-      tweetBtn.disabled = true;
-      tweetBtn.classList.remove("able");
-      counter.style.color = "red";
-    } else {
-      tweetBtn.disabled = true;
-      tweetBtn.classList.remove("able");
-      counter.style.color = "rgb(80,182,245)";
-    }
-  })
-  
-})
+//  $postsContainer.prepend('<div class="card del-post mt-3"><div class="card-header btn-yellowLab"><small>Publicado por</small> Usuario</div><div class="card-body" id="appendLike"><p class="card-text new-post"></p></div></div>');
+//  $('.new-post').first().append($textArea.val());
+//  $('#appendLike').append('<button class="btn btn-secondary like-btn"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Me gusta</button>');
+//  $('.card-header').first().prepend('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+//  $textArea.val('');
+//  $textArea.focus();
+//  $postBtn.attr('disabled', true);
 
+
+});
